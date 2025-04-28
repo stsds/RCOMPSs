e0b341cb8cf9ba6cc8c13bd46a8269aba392c849
Xiran-Zhang <xiran.zhang@kaust.edu.sa>
2025-04-28 13:10:40 +0300

    Add LICENSE and deleted other/

--- aux/executor.R
+++ aux/executor.R
@@ -1,20 +1,14 @@
 #!/usr/bin/env Rscript
-#
-#  Copyright 2002-2025 Barcelona Supercomputing Center (www.bsc.es)
-#            2023-2025 King Abdullah University of Science and Technology (www.kaust.edu.sa)
-#
-#  Licensed under the Apache License, Version 2.0 (the "License");
-#  you may not use this file except in compliance with the License.
-#  You may obtain a copy of the License at
-#
-#      http://www.apache.org/licenses/LICENSE-2.0
-#
-#  Unless required by applicable law or agreed to in writing, software
-#  distributed under the License is distributed on an "AS IS" BASIS,
-#  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-#  See the License for the specific language governing permissions and
-#  limitations under the License.
-#
+
+# Copyright (c) 2025- King Abdullah University of Science and Technology,
+# All rights reserved.
+# RCOMPSs is a software package, provided by King Abdullah University of Science and Technology (KAUST) - STSDS Group.
+
+# @file executor.R
+# @brief This file is the executor
+# @version 1.0
+# @author Xiran Zhang
+# @date 2025-04-28
 
 # ##################################### #
 # ######### RCOMPSs EXECUTOR ########## #
--- aux/r_piper.sh
+++ aux/r_piper.sh
@@ -1,5 +1,15 @@
 #!/bin/bash
 
+# Copyright (c) 2025- King Abdullah University of Science and Technology,
+# All rights reserved.
+# RCOMPSs is a software package, provided by King Abdullah University of Science and Technology (KAUST) - STSDS Group.
+
+# @file r_piper.R
+# @brief This file #TODO
+# @version 1.0
+# @author Xiran Zhang
+# @date 2025-04-28
+
 SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 
 ########################################
